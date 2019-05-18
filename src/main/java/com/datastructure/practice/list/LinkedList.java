9d0300aa0285231c422afb9e7b02c22b79620f78
yangsiqin <yanghehe333@gmail.com>
2019-05-18 15:37:57 +0800

    add LinkedList

---
+++ src/main/java/com/datastructure/practice/list/LinkedList.java
@@ -0,0 +1,130 @@
+package com.datastructure.practice.list;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+public class LinkedList<E> implements List<E> {
+    private Node<E> first = null;
+    private int listLength = 0;
+
+    @Override
+    public boolean add(E e) {
+        if(first == null) {
+            first = new Node<>(e,null,first);
+            listLength++;
+            return true;
+        }
+        Node<E> node = first;
+        while (node.getNext() != null) {
+            node = node.getNext();
+        }
+        node.setNext(new Node<>(e,null,node));
+        listLength++;
+        return true;
+    }
+
+    @Override
+    public E get(int index) {
+        if(index >= listLength || index < 0) {
+            throw new IllegalArgumentException("index is large than max index ");
+        }
+        Node<E> target = first;
+        int currIndex = 0;
+        while (currIndex < index){
+            target = target.getNext();
+            currIndex++;
+        }
+        return target.getData();
+    }
+
+    @Override
+    public int indexOf(E e) {
+        int targetIndex = -1;
+        int tempIndex = -1;
+        Node<E> target = first;
+        while (target != null) {
+            tempIndex++;
+            if(e == null) {
+                if(target.getData() == null) {
+                    targetIndex = tempIndex;
+                    break;
+                }
+            }else {
+                if(e.equals(target.getData())){
+                    targetIndex = tempIndex;
+                    break;
+                }
+            }
+            target = target.getNext();
+        }
+        return targetIndex;
+    }
+
+    @Override
+    public boolean add(int index, E e) {
+        if(index > listLength) {
+            throw new IllegalArgumentException("index is large than max index.");
+        }
+        if(index == listLength) {
+            return add(e);
+        }
+        Node<E> node = first;
+        for (int i = 1; i < index; i++) {
+            node = node.getNext();
+        }
+        Node<E> newNode = new Node<>(e,node.getNext(),node);
+        node.next.prev = newNode;
+        node.setNext(newNode);
+        return true;
+    }
+
+    @Override
+    public E remove(int index) {
+        if(index > listLength - 1) {
+            throw new IllegalArgumentException("index is large than max index.");
+        }
+        if(index == 0) {
+            E data = first.getData();
+            first = first.getNext();
+            listLength--;
+            return data;
+        }
+        Node<E> node = first;
+        for (int i = 1; i < index; i++) {
+            node = node.getNext();
+        }
+        Node<E> shouldRemove = node.getNext();
+        node.setNext(shouldRemove.getNext());
+        if(shouldRemove.next != null){
+            shouldRemove.next.prev = node;
+        }
+        listLength--;
+        return shouldRemove.getData();
+    }
+
+    @Override
+    public int size() {
+        return listLength;
+    }
+
+    @Override
+    public void clear() {
+        Node<E> node = first;
+        while (node != null) {
+            Node<E> next = node.getNext();
+            node.setNext(null);
+            node.setData(null);
+            node = next;
+        }
+        first = null;
+        listLength = 0;
+    }
+
+    @Data
+    @AllArgsConstructor
+    private static class Node<E> {
+        private E data;
+        private Node<E> next;
+        private Node<E> prev;
+    }
+}
---
+++ src/test/java/com/datastructure/practice/list/LinkedListTest.java
@@ -0,0 +1,140 @@
+package com.datastructure.practice.list;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LinkedListTest {
+
+    @Test
+    public void addTest(){
+        List list = new LinkedList<Integer>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        Assert.assertTrue(list.size() == 5);
+        try {
+            list.add(6);
+        }catch (Exception e) {
+            Assert.assertTrue(e instanceof IllegalArgumentException);
+        }
+    }
+
+    @Test
+    public void getTest(){
+        List<Integer> list = new LinkedList<Integer>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        Assert.assertTrue(list.size() == 5);
+        Assert.assertTrue(list.get(0) == 1);
+        Assert.assertTrue(list.get(1) == 2);
+        Assert.assertTrue(list.get(2) == 3);
+        Assert.assertTrue(list.get(3) == 4);
+        Assert.assertTrue(list.get(4) == 5);
+        try {
+            list.get(5);
+        }catch (Exception e) {
+            Assert.assertTrue(e instanceof IllegalArgumentException);
+        }
+    }
+
+    @Test
+    public void indexOfTest(){
+        List<Integer> list = new LinkedList<Integer>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+
+        Assert.assertTrue(list.indexOf(1) == 0);
+        Assert.assertTrue(list.indexOf(2) == 1);
+        Assert.assertTrue(list.indexOf(3) == 2);
+        Assert.assertTrue(list.indexOf(4) == 3);
+        Assert.assertTrue(list.indexOf(5) == 4);
+    }
+
+
+    @Test
+    public void addTest2(){
+        List list = new LinkedList<Integer>();
+        list.add(1);
+        list.add(2);
+        list.add(4);
+        list.add(5);
+        Assert.assertTrue(list.size() == 4);
+        try {
+            list.add(5,6);
+        }catch (Exception e) {
+            Assert.assertTrue(e instanceof IllegalArgumentException);
+        }
+        list.add(2,3);
+        Assert.assertTrue(list.indexOf(1) == 0);
+        Assert.assertTrue(list.indexOf(2) == 1);
+        Assert.assertTrue(list.indexOf(3) == 2);
+        Assert.assertTrue(list.indexOf(4) == 3);
+        Assert.assertTrue(list.indexOf(5) == 4);
+    }
+
+    @Test
+    public void removeTest(){
+        List<Integer> list = new LinkedList<Integer>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+
+        list.remove(4);
+
+        Assert.assertTrue(list.get(0) == 1);
+        Assert.assertTrue(list.get(1) == 2);
+        Assert.assertTrue(list.get(2) == 3);
+        Assert.assertTrue(list.get(3) == 4);
+
+        list.remove(2);
+
+        Assert.assertTrue(list.get(0) == 1);
+        Assert.assertTrue(list.get(1) == 2);
+        Assert.assertTrue(list.get(2) == 4);
+    }
+
+    @Test
+    public void sizeTest(){
+        List<Integer> list = new LinkedList<Integer>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+
+        list.remove(4);
+        Assert.assertTrue(list.size() == 4);
+        list.remove(3);
+        Assert.assertTrue(list.size() == 3);
+        list.remove(2);
+        Assert.assertTrue(list.size() == 2);
+        list.remove(1);
+        Assert.assertTrue(list.size() == 1);
+        list.remove(0);
+        Assert.assertTrue(list.size() == 0);
+    }
+
+
+    @Test
+    public void clearTest(){
+        List<Integer> list = new LinkedList<Integer>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+
+        list.clear();
+        Assert.assertTrue(list.size() == 0);
+    }
+}
